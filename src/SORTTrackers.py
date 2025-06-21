4d2a335e67b3a656f8d686060572979c94caeb77
damspada <spadaccini.1986173@studenti.uniroma1.it>
2025-06-21 08:30:10 +0000

    started Google Colab

--- src/SORTTrackers.py
+++ src/SORTTrackers.py
@@ -3,4 +3,4 @@ class SORTTrackers:
     def __init__(self):
         # The core class that runs the SORT algorithm.
         # Manages all tracks: updates them with matches,
-        # creates new ones, and removes stale tracks.
+        # creates new ones, and removes stale tracks. 
