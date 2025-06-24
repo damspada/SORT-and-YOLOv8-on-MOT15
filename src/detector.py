40760628c471e8fbd37c4264f57dff9d4487eb5f
damspada <spadaccini.1986173@studenti.uniroma1.it>
2025-06-24 13:34:01 +0000

    update detector for tracking only person

--- src/detector.py
+++ src/detector.py
@@ -4,7 +4,11 @@ import numpy as np
 class Detector:
 
     def __init__(self):
-        self.yolo = YOLO("yolov8n.pt") # try with larger models o Fine-Tuning
+        self.model = YOLO("yolov8n.pt") # try with larger models o Fine-Tuning
     
     def get_bounding_box(self, frame: np.ndarray):
-      return self.yolo(frame, conf=0.25)
+      idx_person = self.model.names.index("person")
+      return self.model.predict(frame, classes = [idx_person],conf=0.25)
+    
+    def show_classes(self):
+      print(self.model.names)
