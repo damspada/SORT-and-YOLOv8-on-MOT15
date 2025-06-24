2f983cda5500fc55785ce5e631ab7e1263a8b24e
damspada <spadaccini.1986173@studenti.uniroma1.it>
2025-06-24 14:58:36 +0000

    Matrix definition for KF in Track and Predictor

--- src/predictor.py
+++ src/predictor.py
@@ -1,5 +1,64 @@
+import torch
+from src.track import Track
+
 class Prediction:
 
-    def __init__(self):
-        # Implements the Kalman Filter to predict next object
-        # positions for existing tracks.
+  def __init__(self, dt):
+
+    # acceleration could be inserted
+    self.A = torch.tensor([
+      [1, 0, 0, 0, dt, 0],
+      [0, 1, 0, 0, 0,  dt],
+      [0, 0, 1, 0, 0,  0],
+      [0, 0, 0, 1, 0,  0],
+      [0, 0, 0, 0, 1,  0],
+      [0, 0, 0, 0, 0,  1]
+    ], dtype=torch.float32)
+
+    # It could change after test
+    self.Q = torch.tensor([
+      [4, 0,  0,   0,   0,   0],
+      [0, 4,  0,   0,   0,   0],
+      [0, 0, 100,  0,   0,   0],
+      [0, 0,  0,  100,  0,   0],
+      [0, 0,  0,   0,  100,  0],
+      [0, 0,  0,   0,   0,  100]
+    ], dtype=torch.float32)
+
+    self.H = torch.tensor([
+      [1, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0]
+    ], dtype=torch.float32)
+
+    # It could change after test
+    self.R = torch.tensor([
+      [1, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 1]
+    ], dtype=torch.float32) 
+
+    # def prediction_step(self, person: Track):
+    #   person.X_hat = self.A @ person.X_hat
+    #   person.P = self.A @ person.P @ self.A.T + self.Q
+    
+    # def estimated
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/track.py
+++ src/track.py
@@ -1,5 +1,20 @@
-class Track:
+import torch
 
-    def __init__(self):
-        # Represents a single tracked object with its characteristics
-        # and handles updates (new detections or predictions).
+class Track:
+  _id_counter = 0
+  def __init__(self, X0: torch.Tensor):
+    
+    self.id = _id_counter
+    _id_counter += 1
+
+    V0 = torch.tensor([[1,1]], dtype=torch.float32)
+    self.X_hat = torch.cat((X0, V0), dim=1).T
+
+    self.P = torch.tensor([
+      [9, 0, 0, 0,  0,   0],
+      [0, 9, 0, 0,  0,   0],
+      [0, 0, 4, 0,  0,   0],
+      [0, 0, 0, 4,  0,   0],
+      [0, 0, 0, 0, 900,  0],
+      [0, 0, 0, 0,  0,  900]
+    ])
