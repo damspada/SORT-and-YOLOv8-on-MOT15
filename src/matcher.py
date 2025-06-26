be09b1f306569cbf3dda7d2cfbceb4af7caf5735
damspada <spadaccini.1986173@studenti.uniroma1.it>
2025-06-26 11:56:22 +0000

    Creating a scheme for Hungarian Algorithm

--- src/matcher.py
+++ src/matcher.py
@@ -8,20 +8,20 @@ class Matcher:
   def __init__(self):
     pass
   
-  def _IoU(A: torch.Tensor, B: torch.Tensor) -> float:
-    """
-    Shape of A and B is [x_sx, y_sx, x_dx, y_dx]
-    """
-    intersection_x = max(0, min(A[2], B[2] - max(A[0], B[0])))
-    intersection_y = max(0, min(A[3], B[3] - max(A[1], B[1])))
-    intersection_boxes = intersection_x * intersection_y
-    if intersection_boxes:
-      wA, wB = A[2]-A[0], B[2]-B[0]
-      hA, hB = A[3]-A[1], B[3]-B[1]
-      union_boxes = (wA * hA) + (wB * hB) - intersection_boxes
-      return intersection_boxes/union_boxes
-    else:
-      return intersection_boxes
+  # def _IoU(A: torch.Tensor, B: torch.Tensor) -> float:
+  #   """
+  #   Shape of A and B is [x_sx, y_sx, x_dx, y_dx]
+  #   """
+  #   intersection_x = max(0, min(A[2], B[2] - max(A[0], B[0])))
+  #   intersection_y = max(0, min(A[3], B[3] - max(A[1], B[1])))
+  #   intersection_boxes = intersection_x * intersection_y
+  #   if intersection_boxes:
+  #     wA, wB = A[2]-A[0], B[2]-B[0]
+  #     hA, hB = A[3]-A[1], B[3]-B[1]
+  #     union_boxes = (wA * hA) + (wB * hB) - intersection_boxes
+  #     return intersection_boxes/union_boxes
+  #   else:
+  #     return intersection_boxes
   
   def _IoU_matrix(tracks: torch.Tensor, detections: torch.Tensor) -> torch.Tensor:
     """
@@ -49,10 +49,28 @@ class Matcher:
     detection_areas = (detections[:,:,2] - detections[:,:,0]) * (detections[:,:,2] - detections[:,:,0]) #(1,M)
     union_boxes = tracks_areas * detection_areas - intersection_boxes #(N,M)
 
-    # Hungarian Matrix
-    return intersection_boxes / union_boxes #(N,M)
+    # The Hungarian algorithm minimizes cost, so we use 1 - IoU
+    return torch.ones_like(union_boxes) - (intersection_boxes / union_boxes) #(N,M)
 
-  # def match_tracks_and_detections(self, tracks: torch.Tensor, detections: torch.Tensor):
+  def _rectangle_to_square(matrix: torch.Tensor) -> torch.Tensor:
+    """
+    Trasform a matrix (N,M) in:
+      (N,N) if N > M
+      (M,M) if M > N
+    All new entries are set to 1e6 to discourage assignments by the algorithm
+    """
+
+  def match_tracks_and_detections(self, tracks: torch.Tensor, detections: torch.Tensor):
+    # Step 0 -> Build hungarian matrix (N,N) with the cost
+    H = self._rectangle_to_square(self._IoU_matrix(tracks, detections))
+    # Step 1 -> Subtract from each row the minimum element in it
+    # Step 2 -> Subtract from each column the minimum element in it
+    # Step 3 -> Cross the 0's with the minimum number of lines needed (if N==#lines jump to 5)
+    # Step 4 -> Find the smallest entry not covered by any line
+    #           and subtract this entry to the entire matrix except 0 (jump to 3)
+    # Step 5 -> Assign detections to tracks starting with the line with only one zero,
+    #           do not accept pairs with a cost greater than a threshold.
+    
 
 
 
